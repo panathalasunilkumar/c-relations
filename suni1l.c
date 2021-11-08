823e49ff74443324ae1283fff57e2ce681859674
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-08 12:11:12 +0530

    Add files via upload

---
+++ suni1l.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void main()
+  {  
+     int a, b,c,d,e,f,g,h;
+     a= 20;
+     b=10;
+     
+     
+   printf(" %d > %d --> %d\n",a,b,a>b);
+   printf(" %d >= %d --> %d\n",a,b,a>=b);
+   printf(" %d < %d -->%d\n",a,b,a<b);
+   printf(" %d == %d --> %d\n",a,b,a==b);
+   printf(" %d != %d --> %d\n",a,b,a!=b);
+   printf(" %d <= %d --> %d\n",a,b,a<=b);
+  }
